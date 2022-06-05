164fc86ba5349381979edc0077371594af1790f6
jrhei <jannorheydon@gmail.com>
2022-06-05 11:39:29 +0800

    BETA 0.03 * transfer the shuffling procedure to a private method called self.shuffle * transfer the 12 round procedure to a public class called self.start_as_decoder * create a method to return the shuffled array from the private method self.shuffle * create a procedure that will prompt the user to input choices 1-3 which asks to be a decoder, making the code or exit

--- mastermind.rb
+++ mastermind.rb
@@ -1,21 +1,43 @@
 class Game
-    game_turns = 1
-    colors = ["red", "green", "yellow", "blue", "orange", "white"]
-    color_shuffled = []
+    private
+    def self.shuffle
+        @@colors = ["red", "green", "yellow", "blue", "orange", "white"]
+        @@color_shuffled = []
+    
+        @@color_shuffled = @@colors.shuffle
 
-    color_shuffled = colors.shuffle
-    while color_shuffled.length > 4
-        color_shuffled.pop
+        while @@color_shuffled.length > 4
+            @@color_shuffled.pop
+        end
+    end
+    public
+    def self.start_as_decoder
+        game_turns = 1
+        
+        while game_turns <= 12
+            puts game_turns
+
+            game_turns += 1
+        end
+    end 
+    def self.get_shuffled
+        @@color_shuffled
+    end
+    choice = 0
+    while choice > 3 || choice <= 0
+        print "[1] Be the Decoder\n"
+        print "[2] Create the code\n"
+        print "[3] Exit\n"
+        print "Enter your choice: "
+        choice = gets.chomp.to_i
     end
 
-
-    p color_shuffled
+    if choice == 1
+        self.shuffle
+        p self.get_shuffled
+        self.start_as_decoder
+    end
     
-    while game_turns <= 12
-        puts game_turns
-
-        game_turns += 1
-    end 
 end
 
 g = Game.new
