e50e13e0e190b5c5ba689ec41f985bd0f23211d6
jrhei <jannorheydon@gmail.com>
2022-06-07 10:43:13 +0800

    BETA 0.06
    * Separate the procedure that takes input from a user to a function that can take inputs from a user as a codebreaker or codemaker
    * Create a function that will determine if the game is won

--- mastermind.rb
+++ mastermind.rb
@@ -12,56 +12,61 @@ class Game
         end
     end
     public
+    def self.check_if_winner(hints)
+        correct_hints = ["o", "o", "o", "o"]
+        if hints == correct_hints
+            return true
+        else
+            return false
+        end
+    end
+    def self.start_as_codemaker
+        puts "\n\nWhen the game prompts you, enter the color 'red', 'green', 'yellow', 'blue', 'orange', or 'white' into the corresponding position for your code"
+
+    end
+    def self.take_code
+        prompt = "Input here: "
+        print prompt
+        while user_input = gets.chomp
+            case user_input
+            when "red"
+                return "red"
+            when "green"
+                return "green"
+            when "yellow"
+                return "yellow"
+            when "blue"
+                return "blue"
+            when "orange"
+                return "orange"
+            when "white"
+                return "white"
+            else
+                puts "Invalid input try again."
+                #p player_choice
+                print prompt
+            end
+            
+        end
+        
+    end
     def self.start_as_decoder
         game_turns = 1
         puts "\n\nWhen the game prompts you, enter the color 'red', 'green', 'yellow', 'blue', 'orange', or 'white' into the corresponding position"
+        puts "12 ROUNDS ONLY"
         while game_turns <= 12
             player_choice = []
+            puts "Round: #{game_turns}"
             #puts game_turns
             prompt = "Input here: "
-            print prompt
+            #print prompt
+            #
             length = 0 
-                while length != 4
-                    while user_input = gets.chomp
-                        case user_input
-                        when "red"
-                            player_choice.push("red")
-                            p player_choice
-                            length += 1
-                            break
-                        when "green"
-                            player_choice.push("green")
-                            p player_choice
-                            length += 1
-                            break
-                        when "yellow"
-                            player_choice.push("yellow")
-                            p player_choice
-                            length += 1
-                            break
-                        when "blue"
-                            player_choice.push("blue")
-                            p player_choice
-                            length += 1
-                            break
-                        when "orange"
-                            player_choice.push("orange")
-                            p player_choice
-                            length += 1
-                            break
-                        when "white"
-                            player_choice.push("white")
-                            p player_choice
-                            length += 1
-                            break
-                        else
-                            puts "Invalid input try again."
-                            p player_choice
-                            print prompt
-                        end
-                        
-                    end
-                end
+            while length != 4
+                player_choice.push(self.take_code)
+                p player_choice
+                length += 1
+            end
             p self.get_shuffled
             hints = []
             print "\n------Hint instructions-----\n"
@@ -74,17 +79,23 @@ class Game
 
                     when self.get_shuffled_pos(y)
                         if i == y
-                            hints.push('x')
+                            hints.push('o')
                             
                         else
-                            hints.push('o')
+                            hints.push('x')
                             
                         end
                     end
                 end
             end
             p hints.shuffle
-            game_turns += 1
+            if self.check_if_winner(hints)
+                p "You have guessed the code!"
+                break
+            else
+                game_turns += 1
+            end
+            
         end
         
     end 
@@ -115,3 +126,46 @@ end
 
 g = Game.new
 g
+
+=begin
+    
+                while user_input = gets.chomp
+                    case user_input
+                    when "red"
+                        player_choice.push("red")
+                        p player_choice
+                        length += 1
+                        break
+                    when "green"
+                        player_choice.push("green")
+                        p player_choice
+                        length += 1
+                        break
+                    when "yellow"
+                        player_choice.push("yellow")
+                        p player_choice
+                        length += 1
+                        break
+                    when "blue"
+                        player_choice.push("blue")
+                        p player_choice
+                        length += 1
+                        break
+                    when "orange"
+                        player_choice.push("orange")
+                        p player_choice
+                        length += 1
+                        break
+                    when "white"
+                        player_choice.push("white")
+                        p player_choice
+                        length += 1
+                        break
+                    else
+                        puts "Invalid input try again."
+                        p player_choice
+                        print prompt
+                    end
+                    
+                end
+=end
